1f1fe6041191a7366499b5d7a1d7b69a1fca83d2
Tatsuya1117 <othe.world.pc@gmail.com>
2022-08-08 00:00:54 +0900

    linter check

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -16,57 +16,57 @@ const PER_PAGE = 5;
 export default function Article({ article }: Props) {
   return (
     <>
-    <SEO
-      title={article.title}
-      description={article.description}
-      keyword={article.keyword}
-      image={article.eye_catch.url}
-      url={`/article/${article.id}`}
-    />
-    <div className={styles.detailArea}>
-      <Link href={"/"}>
-        <a>
-          <h2 className={styles.detailMainTitle}>Topics</h2>
-        </a>
-      </Link>
-      <div className={styles.detailMainArea}>
-        <div className="">
-          <div className={styles.detailSubTitle}>{article.title}</div>
-        </div>
-        {article.tag && (
-          <div className="">
-            <div className={styles.detailTag}>#{article.tag}</div>
-          </div>
-        )}
-        <div className={styles.detailStyleImage}>
-          <Image
-            className=""
-            src={article.eye_catch.url}
-            objectFit="contain"
-            alt={article.title + "の画像です"}
-            width={960}
-            height={960}
-          />
-        </div>
-        <div className="">
-          <div className={styles.detailBody1}>
-            {/* Prevent HTML tags from being output */}
-            <div
-              dangerouslySetInnerHTML={{
-                __html: `${article.body}`,
-              }}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.next}>
-        <Link href={"/"} passHref>
-          <a className={styles.nextInner}>
-            <span className={styles.nextInnerIn}>Topに戻る</span>
+      <SEO
+        title={article.title}
+        description={article.description}
+        keyword={article.keyword}
+        image={article.eye_catch.url}
+        url={`/article/${article.id}`}
+      />
+      <div className={styles.detailArea}>
+        <Link href={"/"}>
+          <a>
+            <h2 className={styles.detailMainTitle}>Topics</h2>
           </a>
         </Link>
+        <div className={styles.detailMainArea}>
+          <div className="">
+            <div className={styles.detailSubTitle}>{article.title}</div>
+          </div>
+          {article.tag && (
+            <div className="">
+              <div className={styles.detailTag}>#{article.tag}</div>
+            </div>
+          )}
+          <div className={styles.detailStyleImage}>
+            <Image
+              className=""
+              src={article.eye_catch.url}
+              objectFit="contain"
+              alt={article.title + "の画像です"}
+              width={960}
+              height={960}
+            />
+          </div>
+          <div className="">
+            <div className={styles.detailBody1}>
+              {/* Prevent HTML tags from being output */}
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `${article.body}`,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.next}>
+          <Link href={"/"} passHref>
+            <a className={styles.nextInner}>
+              <span className={styles.nextInnerIn}>Topに戻る</span>
+            </a>
+          </Link>
+        </div>
       </div>
-    </div>
     </>
   );
 }
