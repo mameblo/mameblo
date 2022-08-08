42a8bc535b928cd63a503a24522d1908f5ea7f3b
Tatsuya1117 <othe.world.pc@gmail.com>
2022-08-08 19:39:04 +0900

    fixed sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,13 @@
 // next-sitemap.js
-/** @type {import('next-sitemap').IConfig} */
+/** @type {import("next-sitemap").IConfig} */
 
 module.exports = {
-  siteUrl: `${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl: `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
+  generateRobotsTxt: true,
+  exclude: ["/server-sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}` + "/server-sitemap.xml",
+    ],
+  },
 };
