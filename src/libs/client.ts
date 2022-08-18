842bf1e5831712639a33d374ce90fc044979d4fb
kanomame23 <kanomame23@github.com>
2022-08-18 17:09:49 +0900

    first commit

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: 'goodday-corporate',  // service-domainとは https:// 以降、自分で設定した箇所を指す
+  serviceDomain: 'mameblo',  // service-domainとは https:// 以降、自分で設定した箇所を指す
   // null合体
   apiKey: process.env.NEXT_PUBLIC_API_KEY ?? '',
 });
