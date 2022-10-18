2996d21af4acceb9f10f366f7df69899d05f0637
kanomame23 <kanomame23@github.com>
2022-10-18 14:38:02 +0900

    テストプッシュ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home({ articles }: Props) {
         {/* <Illust /> */}
         <LightBox />
         <div className={styles.articleArea}>
-          <h2 className={styles.articleMainTitle}>Blog</h2>
+          <h2 className={styles.articleMainTitle}>Blog●</h2>
           <p className={styles.articleSubTitle}>ブログ</p>
           {articles.map((article) => (
             <div className={styles.articleSingle} key={article.id}>
