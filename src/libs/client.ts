69f7d788a9eb14982a7482696def0df42d82e712
kanomame23 <yk.kanonhamada@gmail.com>
2022-12-08 16:46:29 +0900

    APIキーを隠蔽

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -4,5 +4,5 @@ import { createClient } from "microcms-js-sdk";
 export const client = createClient({
   serviceDomain: "mameblo", // service-domainとは https:// 以降、自分で設定した箇所を指す
   // null合体
-  apiKey: process.env.NEXT_PUBLIC_API_KEY ?? "",
+  apiKey: process.env.API_KEY ?? "",
 });
